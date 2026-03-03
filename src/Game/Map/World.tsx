7a999c517af931cd89712a75ecf6d6d153bbde92
Tommi-K <tomkrmelj@proton.me>
2026-03-03 20:51:55 +0100

    3D terrain

--- src/Game/Map/World.tsx
+++ src/Game/Map/World.tsx
@@ -7,46 +7,67 @@ function App() {
     return (
         <div style={{ height: '100vh', width: '100vw', backgroundColor: '#000' }}>
         <Map
-            initialViewState={{
-                longitude: 0,
-                latitude: 0,
-                zoom: 3.5
-            }}
-            minZoom={2.25}
-            maxZoom={16}
-            doubleClickZoom={false}
+        initialViewState={{
+            longitude: 0,
+            latitude: 0,
+            zoom: 3.5
+        }}
+        minZoom={2.25}
+        maxZoom={16}
+        doubleClickZoom={false}
 
-            maxBounds={[
-                [-Infinity, -80],
-                [Infinity, 85]
-            ]}
+        maxBounds={[
+            [-Infinity, -80],
+            [Infinity, 85]
+        ]}
 
-            cursor="default"
-            attributionControl={false}
+        cursor="default"
+        attributionControl={false}
 
-            dragRotate={false}
-            touchPitch={false}
-            pitchWithRotate={false}
+        dragRotate={false}
+        touchPitch={false}
+        pitchWithRotate={false}
+        dragPan={true}
 
-            mapStyle={{
-                version: 8,
-                sources: {
-                    'satellite': {
-                        type: 'raster',
-                tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
-                tileSize: 256
-                    }
+        reuseMaps
+        fadeDuration={0}
+        collectResourceTiming={false}
+
+        terrain={{
+            source: 'terrain-source',
+            exaggeration: 30
+        }}
+
+        mapStyle={{
+            version: 8,
+            sources: {
+                'satellite': {
+                    type: 'raster',
+            tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
+            tileSize: 256
                 },
-                layers: [{
-                    id: 'satellite-layer',
-                type: 'raster',
-                source: 'satellite'
-                }]
-            }}
-            >
 
-            <WorldMap />
+                'terrain-source': {
+                type: 'raster-dem',
+            tiles: ['https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png'],
+            encoding: 'terrarium',
+            tileSize: 256,
+            maxzoom: 5
+            }
+            },
+            layers: [
+                {
+                    id: 'satellite-layer',
+            type: 'raster',
+            source: 'satellite'
+                }
+            ]
+        }}
+        >
+
+        <WorldMap />
         </Map>
+
         </div>
     );
 }
